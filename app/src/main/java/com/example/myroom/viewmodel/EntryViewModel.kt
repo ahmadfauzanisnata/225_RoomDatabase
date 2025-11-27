677f411702b0ecd8b82aabb91688af293ca3ed3a
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-27 12:21:08 +0700

    memBuat ViewModel untuk halaman entri siswa

--- app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
@@ -1,2 +1,12 @@
 package com.example.myroom.viewmodel
 
+
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
+        private set
+
+    /* Fu
