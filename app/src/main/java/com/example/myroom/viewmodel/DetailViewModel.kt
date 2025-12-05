0f9ca6f8209abbf140127f614dcad5bebc057c7c
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-12-05 17:16:02 +0700

    Implementasi DetailViewModel untuk halaman detail siswa

---
+++ app/src/main/java/com/example/myroom/viewmodel/DetailViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myroom.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myroom.repositori.RepositoriSiswa
+import com.example.myroom.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel(){
