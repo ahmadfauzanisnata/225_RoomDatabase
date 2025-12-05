6bb697fdcfccaa5e52463ee83bc1d3d8cd1e75f2
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-12-05 17:11:47 +0700

    Implementasi EditViewModel untuk halaman edit data

---
+++ app/src/main/java/com/example/myroom/viewmodel/EditViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.myroom.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import com.example.myroom.repositori.RepositoriSiswa
+import com.example.myroom.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
